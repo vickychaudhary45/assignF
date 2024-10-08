44e856bf7e3a5006b1e7c552736598f3185a9b07
Vicky Poonia <vickychaudhary4596@gmail.com>
2024-10-08 11:07:27 +0530

    vicky

--- scripts/after_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-cd /home/corp/b2b_frontend_v3
-docker --version
-docker-compose down || echo "no contanier to down"
-aws s3 cp s3://deploymentbucket-feedback-prod/b2b_prod/production/b2b-frontend/.env .
-chown corp.corp .env
-docker-compose up  -d --build
